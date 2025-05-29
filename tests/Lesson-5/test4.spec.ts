5baff140f3e8ea38a0ee8171c61d5022cc0a1f1d
Chi Chi <quynhchi11071996@gmail.com>
2025-05-29 22:56:44 +0700

    feat: Lession 5 - File key takeaway

--- tests/Lesson-5/test4.spec.ts
+++ tests/Lesson-5/test4.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "@playwright/test";
 
-const dataVNExpress: {title: string; content:string}[] = []
+const dataVNExpress: { title: string; content: string }[] = [];
 
 test("Personal notes", async ({ page }) => {
   await test.step("Get data from VNExpress", async () => {
@@ -21,10 +21,24 @@ test("Personal notes", async ({ page }) => {
     }
     console.log("dataVNExpress: ", dataVNExpress);
   });
-  // await test.step("Navigate to Personal Notes Page", async () => {
-  //   await page.goto("https://material.playwrightvn.com/");
-  //   await page
-  //     .locator("//a[contains(text(),'Bài học 4: Personal notes')]")
-  //     .click();
-  // });
+  await test.step("Navigate to Personal Notes Page", async () => {
+    await page.goto("https://material.playwrightvn.com/");
+    await page
+      .locator("//a[contains(text(),'Bài học 4: Personal notes')]")
+      .click();
+  });
+  await test.step("Add 10 notes", async () => {
+    for (let i = 0; i < dataVNExpress.length; i++) {
+      await page
+        .locator("//input[@id='note-title']")
+        .fill(dataVNExpress[i].title);
+      await page
+        .locator("//textarea[@id='note-content']")
+        .fill(dataVNExpress[i].content);
+      await page.locator("//button[@id='add-note']").click();
+    }
+  });
+  await test.step("Search with note title", async () => {
+    await page.locator("//input[@id='search']").fill(dataVNExpress[1].title);
+  });
 });
